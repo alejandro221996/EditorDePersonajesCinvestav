4eb33b666b856f400c89080c2b3a017599da7edb
Manuel Alejandro Juarez Rodriguez <alejandro1996221@gmail.com>
2020-01-21 16:05:45 -0600

    SegundaPrueba
    21/01/2020 16:05 p.m

---
+++ Proyecto/Prueba2/Assets/Scripts/FemaleScripts/SelectGenderFemale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectGenderFemale : MonoBehaviour
+{
+  public GameObject Female;
+  public GameObject Male;
+  public void SelectFemale()
+  {
+        if(Female!= null)
+      {
+          bool isActive= Female.activeSelf;
+
+          Female.SetActive(!isActive);
+      }
+  }
+}
