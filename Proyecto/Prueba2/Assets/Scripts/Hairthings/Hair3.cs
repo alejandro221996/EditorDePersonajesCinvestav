5c74e1b653e58d6ac06c3cfbc5e534decce8aeb5
Manuel Alejandro Juarez Rodriguez <alejandro1996221@gmail.com>
2020-01-21 15:18:22 -0600

    Test1
    21/01/2020

---
+++ Proyecto/Prueba2/Assets/Scripts/Hairthings/Hair3.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hair3 : MonoBehaviour
+{
+  public GameObject H3;
+
+  public void ShowH3()
+  {
+       if(H3!= null)
+      {
+          bool isActive= H3.activeSelf;
+
+          H3.SetActive(!isActive);
+      }
+  }
+}
---
+++ Proyecto/Prueba2/Assets/Scripts/Hairthings/SelectGenderMale.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectGenderMale : MonoBehaviour
+{
+   public GameObject Male;
+
+   public void SelectMale()
+   {
+        if(Male!= null)
+      {
+          bool isActive= Male.activeSelf;
+
+          Male.SetActive(!isActive);
+      }
+   }
+}
