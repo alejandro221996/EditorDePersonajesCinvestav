2132180cb695736c9a5e89c1d8765ffbbd425cd1
Manuel Alejandro Juarez Rodriguez <alejandro1996221@gmail.com>
2020-01-22 13:14:20 -0600

    VersionEstable

---
+++ Prueba2/Assets/Scripts/MaleScripts/Hairthings/HairScripts/Hairs.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Hairs : MonoBehaviour
+{
+    public GameObject[] Hair;
+    public Vector3 StartPosition;
+    public Vector3 CharacterHairPosition;
+    public void ShowHairs()
+    {
+        //Arreglo para mostrar los botones
+        foreach (GameObject item in Hair)
+        {
+            if (item != null)
+            {
+                item.SetActive(true);
+                item.transform.position = StartPosition;
+          
+            }
+
+        } //------------------------------------------------
+    }
+    public void ButtonPress(int ButtonID,GameObject PressedButton)
+    { 
+    if(ButtonID==1)
+        {
+            PressedButton.SetActive(true);
+            
+        }
+    }
+}
+
