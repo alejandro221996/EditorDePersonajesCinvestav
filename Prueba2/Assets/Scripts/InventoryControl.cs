0189e4ac2d81931ebdb6ef045a32134af35a4cdc
Manuel Alejandro Juarez Rodriguez <alejandro1996221@gmail.com>
2020-01-24 17:22:17 -0600

    YaVaQuedando

---
+++ Prueba2/Assets/Scripts/InventoryControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InventoryControl : MonoBehaviour
+{
+    private List<PlayerItem> playerInventory;
+    [SerializeField]
+    private GameObject buttonTemplate;
+    [SerializeField]
+    private GridLayoutGroup gridGroup;
+    [SerializeField]
+    private Sprite[] iconSprites;
+
+    void Start()
+    {
+        playerInventory = new List<PlayerItem>();
+        for (int i = 0; i < iconSprites.Length; i++)
+        {
+            Debug.Log(i);
+            PlayerItem newItem = new PlayerItem();
+            newItem.iconSprite = iconSprites[i];
+            playerInventory.Add(newItem);
+        }
+        GenInventory();
+       
+    }
+
+    void GenInventory()
+    {
+        if (playerInventory.Count < 5)
+        {
+            gridGroup.constraintCount = playerInventory.Count;
+
+        }
+        else
+        {
+            gridGroup.constraintCount = 4;
+        }
+
+        foreach (PlayerItem newItem in playerInventory)
+        {
+           
+            GameObject newButton = Instantiate(buttonTemplate) as GameObject;
+            newButton.SetActive(true);
+           
+            newButton.GetComponent<InventoryButton>().SetIcon(newItem.iconSprite);
+            newButton.transform.SetParent(buttonTemplate.transform.parent, false);
+        }
+    }
+ public class PlayerItem
+    {
+        public Sprite iconSprite;
+    }
+}
---
+++ Prueba2/Assets/Scripts/SelectGender.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectGender : MonoBehaviour
+{
+    public GameObject Male;
+    public GameObject Female;
+    public GameObject SelectMale;
+    public GameObject SelectFemale;
+
+
+    public void ShowAvatar()
+    {
+        if (SelectMale.activeInHierarchy==false && SelectFemale.activeInHierarchy == false)
+        {
+            Male.SetActive(false);
+            Female.SetActive(false);
+            Debug.Log("Entro al primer caso");
+        }
+       if(SelectMale.activeInHierarchy == true)
+            {
+
+           
+                Male.SetActive(true);
+                Debug.Log("Entro al segundo caso");
+            Debug.Log(SelectFemale);
+        }
+        else
+        {
+     
+                Female.SetActive(true);
+                Debug.Log("Entro al tercer caso");
+            Debug.Log(SelectFemale);
+        }
+    }
+}
