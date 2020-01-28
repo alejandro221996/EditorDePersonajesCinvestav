6ed4cf1c7212bca0d26beb690c79746a419868f7
Manuel Alejandro Juarez Rodriguez <alejandro1996221@gmail.com>
2020-01-28 16:52:51 -0600

    VersionEstableConMenusDeHombre

---
+++ Prueba2/Assets/Scripts/MaleScripts/Hairthings/PuttingHairs.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuttingHairs : MonoBehaviour
+{
+    public GameObject Hair1;
+    public GameObject Hair2;
+    public GameObject Hair3;
+    public GameObject Hair4;
+    public GameObject Hair5;
+    public GameObject Hair6;
+    public GameObject Hair7;
+    public GameObject Hair8;
+    public GameObject Hair9;
+    public GameObject Hair10;
+    public GameObject Hair11;
+    public GameObject Hair12;
+    public GameObject Hair13;
+    public GameObject Hair14;
+    public GameObject Hair15;
+
+    public void PutHair(int HairSelected)
+    {
+        switch (HairSelected)
+        {
+            case 1:
+                HideAll();
+                Hair1.SetActive(true);
+
+
+                break;
+            case 2:
+                HideAll();
+                Hair2.SetActive(true);
+
+
+                break;
+            case 3:
+                HideAll();
+                Hair3.SetActive(true);
+
+
+                break;
+            case 4:
+                HideAll();
+                Hair4.SetActive(true);
+
+
+                break;
+            case 5:
+                HideAll();
+                Hair5.SetActive(true);
+
+
+                break;
+            case 6:
+                HideAll();
+                Hair6.SetActive(true);
+
+
+                break;
+            case 7:
+                HideAll();
+                Hair7.SetActive(true);
+
+
+                break;
+            case 8:
+                HideAll();
+                Hair8.SetActive(true);
+
+
+                break;
+            case 9:
+                HideAll();
+                Hair9.SetActive(true);
+
+
+                break;
+            case 10:
+                HideAll();
+                Hair10.SetActive(true);
+
+
+                break;
+            case 11:
+                HideAll();
+                Hair11.SetActive(true);
+
+
+                break;
+            case 12:
+                HideAll();
+                Hair12.SetActive(true);
+
+
+                break;
+            case 13:
+                HideAll();
+                Hair13.SetActive(true);
+
+
+                break;
+            case 14:
+                HideAll();
+                Hair14.SetActive(true);
+
+
+                break;
+            case 15:
+                HideAll();
+                Hair15.SetActive(true);
+
+
+                break;
+            default:
+                HideAll();
+                break;
+
+
+        }
+    }
+    public void HideAll()
+    {
+        Hair1.SetActive(false);
+        Hair2.SetActive(false);
+        Hair3.SetActive(false);
+        Hair4.SetActive(false);
+        Hair5.SetActive(false);
+        Hair6.SetActive(false);
+        Hair7.SetActive(false);
+        Hair8.SetActive(false);
+        Hair9.SetActive(false);
+        Hair10.SetActive(false);
+        Hair11.SetActive(false);
+        Hair12.SetActive(false);
+        Hair13.SetActive(false);
+        Hair14.SetActive(false);
+        Hair15.SetActive(false);
+
+    }
+}
---
+++ Prueba2/Assets/Scripts/MaleScripts/TestingMenus.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingMenus : MonoBehaviour
+{
+    public GameObject BootsMenu;
+    public GameObject PantsMenu;
+    //public GameObject LegsButton;
+    public GameObject BeltMenu;
+    public GameObject UpperArmorMenu;
+    public GameObject GauntletsMenu;
+    public GameObject ShouldersMenu;
+    
+    //public GameObject ArmsButton;
+    public GameObject ArmorMenu;
+    public GameObject TopMenu;
+    public GameObject OuterMenu;
+    public GameObject AccesoriesMenu;
+    
+    //public GameObject BodyButton;
+    public GameObject HairMenu;
+    //public GameObject HairTitle;
+    //public GameObject HairButton;
+    public GameObject AccesoriesHMenu;
+    
+    //public GameObject AccesoriesTitle;
+    public GameObject HatMenu;
+    public GameObject EarsMenu;
+    public GameObject EyesMenu;
+    public GameObject HelmetMenu;
+    //public GameObject HeadButton;
+    public GameObject FacialHairMenu;
+    public GameObject HeadAccesories;
+
+
+    public void MostrarMenu(int MenuSelected)
+    {
+        if (MenuSelected == 1)//--------------------------1=Male
+        { 
+            HairMenu.SetActive(true);
+            AccesoriesHMenu.SetActive(true);
+            //-----HEADHIDE
+            EyesMenu.SetActive(false);
+            EarsMenu.SetActive(false);
+            HelmetMenu.SetActive(false);
+            FacialHairMenu.SetActive(false);
+            HeadAccesories.SetActive(false);
+            HatMenu.SetActive(false);
+         
+            //-----BODYHIDE
+            ArmorMenu.SetActive(false);
+            TopMenu.SetActive(false);
+            OuterMenu.SetActive(false);
+            AccesoriesMenu.SetActive(false);
+            //-----LEGSHIDE
+            UpperArmorMenu.SetActive(false);
+            BootsMenu.SetActive(false);
+            PantsMenu.SetActive(false);
+            BeltMenu.SetActive(false);
+            //-----ARMHIDE
+            GauntletsMenu.SetActive(false);
+            ShouldersMenu.SetActive(false);
+        }
+
+        if (MenuSelected == 2)//---------------------2=Female
+        {
+            EyesMenu.SetActive(true);
+            EarsMenu.SetActive(true);
+            HelmetMenu.SetActive(true);
+            FacialHairMenu.SetActive(true);
+            HeadAccesories.SetActive(true);
+            HatMenu.SetActive(true);
+            //-----HAIRHIDE
+            HairMenu.SetActive(false);
+            AccesoriesHMenu.SetActive(false);
+            //-----BODYHIDE
+            ArmorMenu.SetActive(false);
+            TopMenu.SetActive(false);
+            OuterMenu.SetActive(false);
+            AccesoriesMenu.SetActive(false);
+            //-----LEGSHIDE
+            UpperArmorMenu.SetActive(false);
+            BootsMenu.SetActive(false);
+            PantsMenu.SetActive(false);
+            BeltMenu.SetActive(false);
+            //-----ARMHIDE
+            GauntletsMenu.SetActive(false);
+            ShouldersMenu.SetActive(false);
+
+        }
+        if(MenuSelected==3)
+        {
+            ArmorMenu.SetActive(true);
+            TopMenu.SetActive(true);
+            OuterMenu.SetActive(true);
+            AccesoriesMenu.SetActive(true);
+            //------HEADHIDE
+            EyesMenu.SetActive(false);
+            EarsMenu.SetActive(false);
+            HelmetMenu.SetActive(false);
+            FacialHairMenu.SetActive(false);
+            HeadAccesories.SetActive(false);
+            HatMenu.SetActive(false);
+            //-----LEGSHIDE
+            UpperArmorMenu.SetActive(false);
+            BootsMenu.SetActive(false);
+            PantsMenu.SetActive(false);
+            BeltMenu.SetActive(false);
+            //-----ARMHIDE
+            GauntletsMenu.SetActive(false);
+            ShouldersMenu.SetActive(false);
+            //-----HAIRHIDE
+            HairMenu.SetActive(false);
+            AccesoriesHMenu.SetActive(false);
+
+        }
+        if (MenuSelected==4)
+        {
+            
+            GauntletsMenu.SetActive(true);
+            ShouldersMenu.SetActive(true);
+            //-----HAIRHIDE
+            HairMenu.SetActive(false);
+            AccesoriesHMenu.SetActive(false);
+            //-----BODYHIDE
+            ArmorMenu.SetActive(false);
+            TopMenu.SetActive(false);
+            OuterMenu.SetActive(false);
+            AccesoriesMenu.SetActive(false);
+            //-----LEGSHIDE
+            UpperArmorMenu.SetActive(false);
+            BootsMenu.SetActive(false);
+            PantsMenu.SetActive(false);
+            BeltMenu.SetActive(false);
+            //------HEADHIDE
+            EyesMenu.SetActive(false);
+            EarsMenu.SetActive(false);
+            HelmetMenu.SetActive(false);
+            FacialHairMenu.SetActive(false);
+            HeadAccesories.SetActive(false);
+            HatMenu.SetActive(false);
+        }
+        if(MenuSelected==5)
+        {
+            UpperArmorMenu.SetActive(true);
+            BootsMenu.SetActive(true);
+            PantsMenu.SetActive(true);
+            BeltMenu.SetActive(true);
+            //-----HAIRHIDE
+            HairMenu.SetActive(false);
+            AccesoriesHMenu.SetActive(false);
+            //-----BODYHIDE
+            ArmorMenu.SetActive(false);
+            TopMenu.SetActive(false);
+            OuterMenu.SetActive(false);
+            AccesoriesMenu.SetActive(false);
+            //------HEADHIDE
+            EyesMenu.SetActive(false);
+            EarsMenu.SetActive(false);
+            HelmetMenu.SetActive(false);
+            FacialHairMenu.SetActive(false);
+            HeadAccesories.SetActive(false);
+            HatMenu.SetActive(false);
+            //-----ARMHIDE
+            GauntletsMenu.SetActive(false);
+            ShouldersMenu.SetActive(false);
+
+        }
+        Debug.Log(MenuSelected);
+    }
+}
